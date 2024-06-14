ad2e0250a1878cccee7c5f449712273a15045b75
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2024-06-15 04:59:50 +0700

    Update DatabaseUtil.java

--- Server/src/main/java/DatabaseUtil.java
+++ Server/src/main/java/DatabaseUtil.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 public class DatabaseUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/FileSenderDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "daf3adk$$w";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
