b9c524b8fede85fc6e66a35cfcc8986566efab2a
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2024-06-15 05:02:28 +0700

    Update DatabaseConnection.java

--- Login/src/DatabaseConnection.java
+++ Login/src/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=UserDatabase;encrypt=true;trustServerCertificate=true";
     private static final String USER = "sa";
-    private static final String PASSWORD = "1";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
