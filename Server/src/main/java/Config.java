1cbd6972ea7d730c34c13d02d0a4e845f0663966
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2024-06-15 16:32:39 +0700

    Update Config.java

--- Server/src/main/java/Config.java
+++ Server/src/main/java/Config.java
@@ -3,6 +3,6 @@ public class Config {
     protected static final int PORT = 12345;
     protected static final String DB_URL = "jdbc:mysql://localhost:3306/FileSenderDB";
     protected static final String DB_USER = "root";
-    protected static final String DB_PASSWORD = "123456";
+    protected static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
 }
