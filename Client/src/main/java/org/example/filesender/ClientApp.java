ebb806dea5acfdf5107eabfb2e84ee19c8eb3ddc
tu <daxd2205@gmail.com>
2024-06-16 17:41:16 +0700

    fixed UI: added window minHeight minWidth, textboxes minWidth maxWidth and adjusted Anchor size for proper resizing

--- Client/src/main/java/org/example/filesender/ClientApp.java
+++ Client/src/main/java/org/example/filesender/ClientApp.java
@@ -1,11 +1,13 @@
 package org.example.filesender;
 
-import javafx.application.Application;
+import  javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+
 
 public class ClientApp extends Application {
     public static void main(String[] args) {
@@ -16,10 +18,12 @@ public class ClientApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("Login");
         primaryStage.getIcons().add(new Image("file:src/main/resources/org/example/filesender/images/icon.png"));
         primaryStage.setScene(scene);
+        primaryStage.setMinWidth(650);
+        primaryStage.setMinHeight(400);
         primaryStage.show();
     }
 }
--- Client/src/main/java/org/example/filesender/LoginController.java
+++ Client/src/main/java/org/example/filesender/LoginController.java
@@ -60,6 +60,8 @@ public class LoginController {
             Stage stage = new Stage();
             stage.setTitle("Register");
             stage.setScene(new Scene(root));
+            stage.setMinWidth(600);
+            stage.setMinHeight(400);
             stage.show();
         } catch (IOException e) {
             Logger.getLogger(e.getMessage());
