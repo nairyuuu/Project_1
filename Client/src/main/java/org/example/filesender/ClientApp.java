b39e5ab78b9b90fbb8c6efb5992e722a213deee1
tu <daxd2205@gmail.com>
2024-06-16 17:41:34 +0700

    fixed UI: added window minHeight minWidth, textboxes minWidth maxWidth and adjusted Anchor size for proper resizing

--- Client/src/main/java/org/example/filesender/ClientApp.java
+++ Client/src/main/java/org/example/filesender/ClientApp.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
 
 
 public class ClientApp extends Application {
