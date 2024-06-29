d76aebe7c39dd7aebc7b022d9578ba14c810a449
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2024-06-29 16:52:58 +0700

    Add files via upload

---
+++ Server/src/main/java/org/example/FileServer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+public class FileServer {
+    static boolean running = true;
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(Config.PORT)) {
+            while (running) {
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            Logger.getLogger(e.getMessage());
+        }
+    }
+}
