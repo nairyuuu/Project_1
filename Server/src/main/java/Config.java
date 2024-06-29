d396fe574ec7341dc4b6915210fd239b704c9a05
Lê Long <133205729+nairyuuu@users.noreply.github.com>
2024-06-29 16:51:54 +0700

    Delete Server directory

--- Server/src/main/java/Config.java
+++
@@ -1,8 +0,0 @@
-public class Config {
-    protected static final String UPLOAD_DIR = "uploads/";
-    protected static final int PORT = 12345;
-    protected static final String DB_URL = "jdbc:mysql://localhost:3306/FileSenderDB";
-    protected static final String DB_USER = "root";
-    protected static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
-
-}
